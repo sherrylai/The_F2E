0dfed82073df83ce92974040827b757d39079fdb
sherrylai <sherrylai0903@gmail.com>
2018-06-25 01:31:52 +0800

    0624

--- No3.admin_order/js/admin_order.js
+++ No3.admin_order/js/admin_order.js
@@ -11,6 +11,7 @@ data.addColumn('date', 'Month');
 data.addColumn('number', "Average Temperature");
 data.addColumn('number', "Average Hours of Daylight");
 
+
 data.addRows([
   [new Date(2014, 0),  -.5,  5.7],
   [new Date(2014, 1),   .4,  8.7],
@@ -30,7 +31,7 @@ var materialOptions = {
   chart: {
 	// title: 'Average Temperatures and Daylight in Iceland Throughout the Year'
   },
-  width: 1000,
+  width: 900,
   height: 500,
   series: {
 	// Gives each series an axis name that matches the Y-axis below.
@@ -46,46 +47,46 @@ var materialOptions = {
   }
 };
 
-// var classicOptions = {
-// //   title: 'Average Temperatures and Daylight in Iceland Throughout the Year',
-//   width: 900,
-//   height: 500,
-//   // Gives each series an axis that matches the vAxes number below.
-//   series: {
-// 	0: {targetAxisIndex: 0},
-// 	1: {targetAxisIndex: 1}
-//   },
-//   vAxes: {
-// 	// Adds titles to each axis.
-// 	0: {title: 'Temps (Celsius)'},
-// 	1: {title: 'Daylight'}
-//   },
-//   hAxis: {
-// 	ticks: [new Date(2014, 0), new Date(2014, 1), new Date(2014, 2), new Date(2014, 3),
-// 			new Date(2014, 4),  new Date(2014, 5), new Date(2014, 6), new Date(2014, 7),
-// 			new Date(2014, 8), new Date(2014, 9), new Date(2014, 10), new Date(2014, 11)
-// 		   ]
-//   },
-//   vAxis: {
-// 	viewWindow: {
-// 	  max: 30
-// 	}
-//   }
-// };
+var classicOptions = {
+//   title: 'Average Temperatures and Daylight in Iceland Throughout the Year',
+  width: 900,
+  height: 500,
+  // Gives each series an axis that matches the vAxes number below.
+  series: {
+	0: {targetAxisIndex: 0},
+	1: {targetAxisIndex: 1}
+  },
+  vAxes: {
+	// Adds titles to each axis.
+	0: {title: 'Temps (Celsius)'},
+	1: {title: 'Daylight'}
+  },
+  hAxis: {
+	ticks: [new Date(2014, 0), new Date(2014, 1), new Date(2014, 2), new Date(2014, 3),
+			new Date(2014, 4),  new Date(2014, 5), new Date(2014, 6), new Date(2014, 7),
+			new Date(2014, 8), new Date(2014, 9), new Date(2014, 10), new Date(2014, 11)
+		   ]
+  },
+  vAxis: {
+	viewWindow: {
+	  max: 30
+	}
+  }
+};
 
 function drawMaterialChart() {
   var materialChart = new google.charts.Line(chartDiv);
   materialChart.draw(data, materialOptions);
-//   button.innerText = 'Change to Classic';
-//   button.onclick = drawClassicChart;
+  button.innerText = 'Change to Classic';
+  button.onclick = drawClassicChart;
 }
 
-// function drawClassicChart() {
-//   var classicChart = new google.visualization.LineChart(chartDiv);
-//   classicChart.draw(data, classicOptions);
-//   button.innerText = 'Change to Material';
-//   button.onclick = drawMaterialChart;
-// }
+function drawClassicChart() {
+  var classicChart = new google.visualization.LineChart(chartDiv);
+  classicChart.draw(data, classicOptions);
+  button.innerText = 'Change to Material';
+  button.onclick = drawMaterialChart;
+}
 
 drawMaterialChart();
 
